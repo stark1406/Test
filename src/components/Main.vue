a3614442fb42d7e9e6198af86240b41f86610568
stark1406 <stark140696@mail.ru>
2023-02-26 20:19:39 +0300

    Test
    Test

---
+++ src/components/Main.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <button style="margin-bottom: 15px" @click="clickBtn">
+      Изменить данные
+    </button>
+    <Table :columns="columns" :rows="rows" :check="check" />
+  </div>
+</template>
+
+<script>
+import Table from "@/components/Table.vue";
+
+export default {
+  components: {
+    Table,
+  },
+  data() {
+    return {
+      check: true,
+      columns: [],
+      rows: [],
+    };
+  },
+  computed: {},
+  methods: {
+    clickBtn() {
+      this.check = !this.check;
+      this.columns = [];
+      this.rows = [];
+      this.check ? this.loadTable1() : this.loadTable2();
+    },
+    loadTable1() {
+      this.columns = [
+        "00",
+        "01",
+        "02",
+        "03",
+        "04",
+        "05",
+        "06",
+        "07",
+        "08",
+        "09",
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+        "15",
+        "16",
+        "17",
+        "18",
+        "19",
+        "20",
+        "21",
+        "22",
+        "23",
+      ];
+      this.rows = Array.from({ length: 100 }, () => ({
+        data: Array.from({ length: 24 }, () => ({
+          value: Math.floor(Math.random() * 999999),
+        })),
+      }));
+    },
+    loadTable2() {
+      const date = new Date();
+      const month = date.getMonth();
+      const year = date.getFullYear();
+      const daysInMonth = new Date(year, month + 1, 0).getDate();
+      for (let i = 1; i <= daysInMonth; i++) {
+        this.columns.push(i);
+      }
+
+      for (let i = 0; i < 10; i++) {
+        this.rows.push({});
+      }
+    },
+  },
+  created() {
+    this.loadTable1();
+  },
+};
+</script>
---
+++ src/components/Table.vue
@@ -0,0 +1,90 @@
+<template>
+<div style="display: flex; justify-content: center;">
+  <div class="table-container">
+    <table>
+      <thead>
+        <tr>
+          <th :style="styleColumn">#</th>
+          <th v-for="(column, index) in columns" :key="index" :style="styleColumn">
+            {{ column }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(row, rowIndex) in rows" :key="rowIndex">
+          <td :style="styleRow">{{ rowIndex + 1 }}</td>
+          <td :style="styleTd"
+            v-show="check"
+            v-for="(data, columnIndex) in row.data"
+            :key="columnIndex"
+          >
+            <input  style="width: 60px" maxlength="6" :value="data.value" />
+          </td>
+          <td :style="styleTd" v-show="!check" v-for="column in columns">
+            <div class="icon-container">
+              <img src="../assets/logo.png" height="25px" width="25px" alt="icon" />
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Table",
+  props: {
+    columns: {
+      type: Array,
+      default: () => [],
+    },
+    rows: {
+      type: Array,
+      default: () => [],
+    },
+    check: Boolean,
+  },
+  computed: {
+    styleColumn() {
+        return this.check ? 'background: #fffead' : 'background: aqua'
+    },
+    styleRow() {
+        return this.check ? 'background: #fffead' : 'background: aqua'
+    },
+    styleTd() {
+        return this.check ? 'background: aqua' : 'background: #fffead'
+    },
+  }
+};
+</script>
+
+<style lang="scss">
+.table-container {
+  max-height: 500px;
+  overflow: auto;
+  max-width: 1000px;
+  table {
+    width: 1000px;
+    height: 500px;
+    border-collapse: collapse;
+    th,
+    td {
+      border: 1px solid #ccc;
+      padding: 5px;
+      text-align: center;
+    }
+    th {
+      background-color: #eee;
+    }
+  }
+}
+
+.icon-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+</style>
